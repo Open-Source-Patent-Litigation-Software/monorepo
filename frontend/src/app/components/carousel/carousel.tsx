6aab82ecabffd677c37f495c32840b26cb0c9971
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2024-04-28 03:19:17 -0400

    Fixed stats caroseul deployment issue

--- frontend/src/app/components/carousel/carousel.tsx
+++ frontend/src/app/components/carousel/carousel.tsx
@@ -1,17 +1,5 @@
 import React from "react";
-import CarouselRaw from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-
-const Carousel: any = CarouselRaw; // Bypass TypeScript checks with any
 
 export const StatsCarousel = () => {
-
-  return (
-    <Carousel responsive={responsive}>
-      <div>Item 1</div>
-      <div>Item 2</div>
-      <div>Item 3</div>
-      <div>Item 4</div>
-    </Carousel>
-  );
+  return <div>FILL WITH STATS</div>;
 };
