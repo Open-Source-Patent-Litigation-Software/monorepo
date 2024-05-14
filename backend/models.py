875520a53ea6523c5cfa3fb3c7b5301612329b34
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2024-05-14 18:00:40 -0400

    Updated dev docs

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,11 @@
-# models.py
+from datetime import datetime
 
 
 class OrganizationPayment:
     def __init__(
         self,
         id,
-        organization,
+        organization_id,
         payment_method,
         payment_token,
         expires_at,
@@ -13,7 +13,7 @@ class OrganizationPayment:
         updated_at,
     ):
         self.id = id
-        self.organization = organization
+        self.organization_id = organization_id
         self.payment_method = payment_method
         self.payment_token = payment_token
         self.expires_at = expires_at
@@ -25,7 +25,7 @@ class UserPayment:
     def __init__(
         self,
         id,
-        user,
+        user_id,
         payment_method,
         payment_token,
         expires_at,
@@ -33,7 +33,7 @@ class UserPayment:
         updated_at,
     ):
         self.id = id
-        self.user = user
+        self.user_id = user_id
         self.payment_method = payment_method
         self.payment_token = payment_token
         self.expires_at = expires_at
@@ -42,97 +42,145 @@ class UserPayment:
 
 
 class Organization:
-    def __init__(self, id, name, owners, admins, users, phone, features, payment=None):
+    def __init__(self, id, name, phone, features, payment_id):
         self.id = id
         self.name = name
-        self.owners = owners
-        self.admins = admins
-        self.users = users
         self.phone = phone
         self.features = features
-        self.payment = payment
+        self.payment_id = payment_id
+
+
+class OrganizationOwner:
+    def __init__(self, organization_id, user_id):
+        self.organization_id = organization_id
+        self.user_id = user_id
+
+
+class OrganizationAdmin:
+    def __init__(self, organization_id, user_id):
+        self.organization_id = organization_id
+        self.user_id = user_id
+
+
+class OrganizationUser:
+    def __init__(self, organization_id, user_id):
+        self.organization_id = organization_id
+        self.user_id = user_id
 
 
 class User:
     def __init__(
-        self, id, name, email, password, organization, phone=None, payment=None
+        self,
+        id,
+        first_name,
+        last_name,
+        email,
+        password,
+        phone,
+        organization_id,
+        payment_id,
     ):
         self.id = id
-        self.name = name
+        self.first_name = first_name
+        self.last_name = last_name
         self.email = email
         self.password = password
         self.phone = phone
-        self.organization = organization
-        self.payment = payment
+        self.organization_id = organization_id
+        self.payment_id = payment_id
 
 
-class Case:
+class Patent:
     def __init__(
         self,
         id,
         pqai_id,
         name,
-        folder,
+        folder_id,
         description,
-        organization,
-        googlePatentsLink=None,
-        read_permissions=None,
-        write_permissions=None,
+        organization_id,
+        google_patents_link,
     ):
         self.id = id
-        self.pqaiID = pqai_id
+        self.pqai_id = pqai_id
         self.name = name
-        self.folder = folder
+        self.folder_id = folder_id
         self.description = description
-        self.organization = organization
-        self.googlePatentsLink = googlePatentsLink
-        self.read_permissions = read_permissions if read_permissions else []
-        self.write_permissions = write_permissions if write_permissions else []
+        self.organization_id = organization_id
+        self.google_patents_link = google_patents_link
 
-    def has_read_permission(self, user):
-        return user in self.read_permissions or user in self.folder.read_permissions
 
-    def has_write_permission(self, user):
-        return user in self.write_permissions or user in self.folder.write_permissions
+class PatentReadPermission:
+    def __init__(self, patent_id, user_id):
+        self.patent_id = patent_id
+        self.user_id = user_id
+
+
+class PatentWritePermission:
+    def __init__(self, patent_id, user_id):
+        self.patent_id = patent_id
+        self.user_id = user_id
 
 
 class Folder:
-    def __init__(
-        self,
-        id,
-        name,
-        description,
-        organization,
-        users,
-        cases,
-        parent_folder=None,
-        subfolders=None,
-        read_permissions=None,
-        write_permissions=None,
-    ):
+    def __init__(self, id, name, description, organization_id, parent_folder_id):
         self.id = id
         self.name = name
         self.description = description
-        self.organization = organization
-        self.users = users
-        self.cases = cases
-        self.parent_folder = parent_folder
-        self.subfolders = subfolders if subfolders else []
-        self.read_permissions = read_permissions if read_permissions else []
-        self.write_permissions = write_permissions if write_permissions else []
+        self.organization_id = organization_id
+        self.parent_folder_id = parent_folder_id
 
-    def has_read_permission(self, user):
-        if user in self.read_permissions:
-            return True
-        elif self.parent_folder:
-            return self.parent_folder.has_read_permission(user)
-        else:
-            return False
 
-    def has_write_permission(self, user):
-        if user in self.write_permissions:
-            return True
-        elif self.parent_folder:
-            return self.parent_folder.has_write_permission(user)
-        else:
-            return False
+class FolderUser:
+    def __init__(self, folder_id, user_id):
+        self.folder_id = folder_id
+        self.user_id = user_id
+
+
+class FolderPatent:
+    def __init__(self, folder_id, patent_id):
+        self.folder_id = folder_id
+        self.patent_id = patent_id
+
+
+class FolderReadPermission:
+    def __init__(self, folder_id, user_id):
+        self.folder_id = folder_id
+        self.user_id = user_id
+
+
+class FolderWritePermission:
+    def __init__(self, folder_id, user_id):
+        self.folder_id = folder_id
+        self.user_id = user_id
+
+
+class Search:
+    def __init__(self, id, user_id, query, time):
+        self.id = id
+        self.user_id = user_id
+        self.query = query
+        self.time = time
+
+
+class Metric:
+    def __init__(self, id, search_id, patent_id, sentence, match_percentage):
+        self.id = id
+        self.search_id = search_id
+        self.patent_id = patent_id
+        self.sentence = sentence
+        self.match_percentage = match_percentage
+
+
+class UserSearchCount:
+    def __init__(self, user_id, search_count):
+        self.user_id = user_id
+        self.search_count = search_count
+
+
+class SearchAnalytics:
+    def __init__(self, id, user_id, search_id, search_time):
+        self.id = id
+        self.user_id = user_id
+        self.search_id = search_id
+        self.search_time = search_time
