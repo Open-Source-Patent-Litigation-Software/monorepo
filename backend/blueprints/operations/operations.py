3a1480fb52ce09b1a5c54127b9e997b3468bb507
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2024-05-15 18:13:17 -0400

    Updated flask routing to support the contact form, need to still integrate the front end

--- backend/blueprints/operations/operations.py
+++ backend/blueprints/operations/operations.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 
 operations = Blueprint("operations", __name__, template_folder="templates")
-from database.posts import postToList
+from database.posts import postToList, postContactQuery
 
 
 @operations.route("/addToWaitlist", methods=["POST"])
@@ -12,9 +12,24 @@ def addToWaitlist():
 
     if data is None:
         return jsonify({"error": "No JSON data provided"}), 400
-    
-    email = data.get('email')
-    phoneNumber = data.get('phoneNumber')
-    message = postToList(email, phoneNumber)
 
-    return message  # contains JSON response
+    email = data.get("email")
+    phoneNumber = data.get("phoneNumber")
+    response = postToList(email, phoneNumber)
+    return response  # contains JSON response
+
+
+@operations.route("/handleContactQuery", methods=["POST"])
+def handleContactQuery():
+    """Add user to waitlist."""
+    data = request.get_json()
+    if data is None:
+        return jsonify({"error": "No Contact Form JSON data provided"}), 400
+    
+    firstName = data["firstName"]
+    lastName = data["lastName"]
+    email = data["email"]
+    message = data["message"]
+
+    response = postContactQuery(firstName, lastName, email, message)
+    return response
