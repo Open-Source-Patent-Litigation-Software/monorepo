e795415b22f936ca70eed4e66aa581a95a913baf
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2024-05-29 19:04:02 -0400

    Conditional rendering of tools only if logged in

--- frontend/src/app/_components/navbar/navbar.tsx
+++ frontend/src/app/_components/navbar/navbar.tsx
@@ -2,16 +2,23 @@
 import React from "react";
 import Link from "next/link";
 import { StyledNav, StyledUl, StyledLi, LogoText, LoginLink } from "./styles";
-
+import useUserStore from "@/app/_stores/useUserStore";
 interface NavigationProps {
   // Your props here
 }
 
 export const Navbar: React.FC<NavigationProps> = (props) => {
+  const { email, firstName, lastName, phone } = useUserStore((state) => ({
+    email: state.email,
+    firstName: state.firstName,
+    lastName: state.lastName,
+    phone: state.phone,
+  }));
+
   const routes = {
     about: "/about",
     contact: "/contact",
-    // tool: "/tool",
+    ...(email && { tool: "/tool" }),
   };
 
   return (
