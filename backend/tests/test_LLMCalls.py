814ba7053a6d55734b86c6ab7d3af3c8f4f9b4d7
alecpalo <apalo@umich.edu>
2024-06-13 17:13:42 -0700

    testing llm calls

---
+++ backend/tests/test_LLMCalls.py
@@ -0,0 +1,39 @@
+import pytest
+from run import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+# Test the API Route
+def testCitationsInvalidArgs(client):
+    # empty payload
+    emptyPayload = {}
+    noArgsResponse = client.post('/llm/getCitation', json=emptyPayload)
+    assert noArgsResponse.status_code == 400
+
+    # empty string for one of the args
+    emptyArgPayload = {
+        "user": "test",
+        "patentURL": "",
+        "metric_str": "testing"
+    }
+    empytArgResponse = client.post('/llm/getCitation', json=emptyArgPayload)
+    assert empytArgResponse.status_code == 400
+
+    missingArgPayload = {
+        "user": "test",
+        "metric_str": "testing"
+    }
+    missingArgResponse = client.post('/llm/getCitation', json=missingArgPayload)
+    assert missingArgResponse.status_code == 400
+
+    invalidArgTypePayload = {
+        "user": 10,
+        "patentURL": "testing.com",
+        "metric_str": "testing"
+    }
+    invalidArgTypeResponse = client.post('/llm/getCitation', json=invalidArgTypePayload)
+    assert invalidArgTypeResponse.status_code == 400
