c424161417237a0c9b80586d9041d96cd1ebef89
alecpalo <apalo@umich.edu>
2024-06-30 21:25:53 -0400

    removed hanlder

--- backend/utils/logging/logging.py
+++ backend/utils/logging/logging.py
@@ -11,8 +11,3 @@ def setup_logger():
         config = json.load(f)
 
     logging.config.dictConfig(config)
-    queue_handler = logging.getHandlerByName("queue_handler")
-
-    if queue_handler is not None:
-        queue_handler.listener.start()
-        atexit.register(queue_handler.listener.stop)
