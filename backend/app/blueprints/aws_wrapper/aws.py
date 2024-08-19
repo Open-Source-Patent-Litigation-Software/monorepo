e9916251b0e4f3ce42fcdac68500a022bfd46af8
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2024-08-18 23:19:33 -0400

    PDF polling seems to work, let me pivot to word docs.

--- backend/app/blueprints/aws_wrapper/aws.py
+++ backend/app/blueprints/aws_wrapper/aws.py
@@ -1,23 +1,10 @@
 import boto3
-
-
-class AWSFactory:
-    """AWSFactory class to generate AWS clients based on the type of AWS service"""
-
-    def __init__(self, aws_type):
-        """AWSFactory constructor"""
-        self.aws_type = aws_type
-
-    def generate_client(self):
-        """Generates AWS client based on the type of AWS service"""
-        if self.aws_type == "dynamodb":
-            return Dynamo()
-        else:
-            raise ValueError("Invalid AWS type")
+import uuid
 
 
 class Dynamo:
     """
+    An API to interact with AWS DynamoDB designed for Dulany
     Boto3 Documentation for DynamoDB:
     https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html
     """
@@ -106,33 +93,79 @@ class Dynamo:
             print(error)
 
 
+class S3_CLIENT:
+    """An API to interact with AWS S3 designed for Dulany"""
+
+    def __init__(self):
+        """S3 client constructor"""
+        try:
+            self.client = boto3.client("s3")
+            self.resource = boto3.resource("s3")
+        except Exception as e:
+            print(f"Error initializing S3 client: {str(e)}")
+
+    def create_temp_file(self, file_name):
+        """Create a temporary file with a random name"""
+        randomFileName = "".join([str(uuid.uuid4().hex[:10]), file_name])
+        return randomFileName
+
+    def create_bucket(self, bucketPrefix):
+        """Create an S3 bucket"""
+
+        # Get the current region (avoid hardcoding)
+        session = boto3.session.Session()
+        currentRegion = session.region_name
+
+        # The generated bucket name must be between 3 and 63 chars long
+        bucketName = "".join([bucketPrefix, str(uuid.uuid4())])
+
+        bucketResponse = self.resource.create_bucket(
+            Bucket=bucketName,
+            CreateBucketConfiguration={"LocationConstraint": currentRegion},
+        )
+        print(bucketName, currentRegion)
+        return bucketName, bucketResponse
+
+    def postFile(self, bucket_name: str, file_path: str, key: str) -> None:
+        """Post file to S3 bucket"""
+        try:
+            self.client.upload_file(file_path, bucket_name, key)
+        except Exception as error:
+            print("Error uploading file to S3 bucket")
+            print(error)
+
+
 def main():
 
-    # DynamoDB example
-    dynamo = AWSFactory("dynamodb").generate_client()
-    # test_item = {
-    #     "user": "devk@umich.edu",  # Partition key must be present
-    #     "instance_id": "querty12345",
-    #     "patent_id": "12345",
-    #     "patent_data": {
-    #         "title": "test_title",
-    #         "abstract": "test_abstract",
-    #         "inventors": ["test_inventor1", "test_inventor2"],
-    #         "assignees": ["test_assignee1", "test_assignee2"],
-    #         "date": "2021-10-10",
-    #         "url": "test_url",
+    # # DynamoDB example
+    # dynamo = AWSFactory("dynamodb").generate_client()
+    # # test_item = {
+    # #     "user": "devk@umich.edu",  # Partition key must be present
+    # #     "instance_id": "querty12345",
+    # #     "patent_id": "12345",
+    # #     "patent_data": {
+    # #         "title": "test_title",
+    # #         "abstract": "test_abstract",
+    # #         "inventors": ["test_inventor1", "test_inventor2"],
+    # #         "assignees": ["test_assignee1", "test_assignee2"],
+    # #         "date": "2021-10-10",
+    # #         "url": "test_url",
+    # #     },
+    # # }
+    # # dynamodb.postItem("patent_test", test_item)
+    # print("Data added to DynamoDB")
+    # test_item = dynamo.getItem(
+    #     "patent_test",
+    #     {
+    #         "user": "devk@umich.edu",  # Partition key must be present
+    #         "instance_id": "querty12345",
     #     },
-    # }
-    # dynamodb.postItem("patent_test", test_item)
-    print("Data added to DynamoDB")
-    test_item = dynamo.getItem(
-        "patent_test",
-        {
-            "user": "devk@umich.edu",  # Partition key must be present
-            "instance_id": "querty12345",
-        },
-    )
-    print("Data retrieved from DynamoDB", test_item)
+    # )
+    # print("Data retrieved from DynamoDB", test_item)
+
+    # S3 example
+    S3 = S3_CLIENT()
+    S3.create_bucket("dulany-test-bucket")
 
 
 if __name__ == "__main__":
